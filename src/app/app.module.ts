d8336af59e76d7270ba18068c1667efee40067b2
Rogerio <rogeri_22@hotmail.com>
2019-09-06 21:43:37 -0300

    Exemplo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExemploComponent } from './exemplo/exemplo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExemploComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/exemplo/exemplo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'has-exemplo',
+  templateUrl: './exemplo.component.html',
+  styleUrls: ['./exemplo.component.css']
+})
+export class ExemploComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
